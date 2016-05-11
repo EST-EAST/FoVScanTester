a765812c8f85269c6b9e1f85d8264e35e7598c30
Txinto Vaz <txinto@iac.es>
2016-05-11 11:57:28 +0100

    We import autogenerated example of raster to the directory.

---
+++ sweepsupport.py
@@ -0,0 +1,16 @@
+cte_camsource = 0
+cte_verbose = True
+cte_fileprefix = "frame"
+cte_framePath = "00_acquired"
+
+def commandMotor(x,y):
+  dummy=0 # Dummy for avoiding indentation failures
+
+def stepDone():
+  return 1
+
+def motorPositions():
+  a = 1
+  b = 2
+  c = 3
+  return a,b,c
