d681a2b3a3c5b3d2d587866f4c81f71c0e1aa4d6
Txinto Vaz <txinto@iac.es>
2016-05-12 14:14:18 +0100

    Launcher Urls listed before launch.

--- launcher.py
+++ launcher.py
@@ -10,9 +10,14 @@ import urllib
 def import_URL(URL):
     exec urllib.urlopen(URL).read() in globals()
     
-    
-import_URL("http://gatatac.org:5555/sweep_exes.py")
+menurl = "http://gatatac.org:5555/sweep_exes"
+print "The menu chooser will be downloaded from "+menurl+".py"
+print "The list of avaliable algorithms are here: "+menurl
+import_URL(menurl+".py")
 
 if (sweep_id_to_execute != 0):
-    import_URL("http://gatatac.org:5555/sweep_exes/"+str(sweep_id_to_execute)+".py")
+    sweepurl="http://gatatac.org:5555/sweep_exes/"+str(sweep_id_to_execute)
+    print "The algorithm to be downloaded is "+sweepurl+".py"
+    print "It corresponds to the one described at "+sweepurl
+    import_URL(sweepurl+".py")
 
