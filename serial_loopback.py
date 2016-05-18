cd3acca08b5017341732bc13af23691dd9d053d8
Txinto Vaz <txinto@iac.es>
2016-05-18 18:51:02 +0100

    FSM added to project.

--- serial_loopback.py
+++ serial_loopback.py
@@ -9,6 +9,10 @@ Created on Tue May 17 16:44:56 2016
 import serial
 import sweepconfig
 
+import sys
+sys.path.insert(0, './fsm')
+import FoV
+
 cte_serial_port_loopback = sweepconfig.cte_serial_port_loopback
 
 print "Chosen serial port: "+cte_serial_port_loopback
@@ -16,28 +20,6 @@ print "Chosen serial port: "+cte_serial_port_loopback
 cte_stepTime=1000
 cte_timeout = 2000
 
-def readCommand():
-    current_command=""
-    done=False
-    while (done==False):
-        char_read=ser.read(1)
-        if (char_read=='\13'):
-            print "done: "+current_command
-            done=True
-        else:
-            current_command+=char_read
-            print "acum: "+current_command
-    return current_command
-
-def sendResponse(response_lenght):
-    response = ""
-    for i in range (1,response_lenght+1):
-        response += str(i)
-    ser.write(response+'\13'+'\10')
-
-def serialClose():
-    ser.close()             # close port
-    
 ser = serial.Serial(
     port=cte_serial_port_loopback,
     baudrate=9600,
@@ -48,6 +30,37 @@ ser = serial.Serial(
     dsrdtr=False,
     xonxoff=True
 )
+
+FoV.ser = ser
+
+def readCommand():
+    '''
+    current_command=""
+    done=False
+    while (done==False):
+        char_read=sim_ser.read(1)
+        if (char_read=='\13'):
+            print "done: "+current_command
+            done=True
+        else:
+            current_command+=char_read
+            print "acum: "+current_command
+    return current_command
+    '''
+    return FoV.getCtrlCommand()
+
+def sendResponse(response_lenght):
+    response = ""
+    for i in range (1,response_lenght+1):
+        response += str(i)
+    #ser.write(response+'\13'+'\10')
+    FoV.command_tx_buf = response
+    FoV.sendCtrlResponse()
+
+def serialClose():
+    ser.close()             # close port
+    
+
 
 while (True):
     command=readCommand()
