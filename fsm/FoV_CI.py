23350d49fcc2bd89464bc1d0c91e27a2c63934ff
Txinto Vaz <txinto@iac.es>
2016-05-19 19:22:20 +0100

    Do not sync before generate.

--- fsm/FoV_CI.py
+++ fsm/FoV_CI.py
@@ -5,12 +5,9 @@ dre = DRE.DRE()
 
 def obtainVarName( variable ):
     # ['<global>::obtainVarName' begin]
-    for k, v in locals().items():
-
-        if v is variable:
-
-            a_as_str = k
-
+    for k, v in locals().items():
+        if v is variable:
+            a_as_str = k
     return a_as_str
     # ['<global>::obtainVarName' end]
 
