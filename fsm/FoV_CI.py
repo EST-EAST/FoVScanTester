5505f210a22f83bbe9bc64c53dc7aa3355057a53
Txinto Vaz <txinto@iac.es>
2016-05-19 19:15:52 +0100

    Sockets working. Global variables changed to DRE

--- fsm/FoV_CI.py
+++ fsm/FoV_CI.py
@@ -1,7 +1,17 @@
+import DRE
 # ['Common definitions for 'Code items generator'' begin (DON'T REMOVE THIS LINE!)]
 # Generic code items' definitions
-command_rx_buf = ""
-char_read = '\0'
-ser = None
-command_tx_buf = ""
+dre = DRE.DRE()
+
+def obtainVarName( variable ):
+    # ['<global>::obtainVarName' begin]
+    for k, v in locals().items():
+
+        if v is variable:
+
+            a_as_str = k
+
+    return a_as_str
+    # ['<global>::obtainVarName' end]
+
 # ['Common definitions for 'Code items generator'' end (DON'T REMOVE THIS LINE!)]
