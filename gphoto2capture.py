d7ba5809ff09092fda4b8e63bca0880ae3f3161d
solarnet <solarnet@EstServ>
2016-05-25 13:40:06 +0100

    GPhoto2 integration now disables image showing

--- gphoto2capture.py
+++ gphoto2capture.py
@@ -2,7 +2,7 @@ import subprocess
 
 import gphoto2 as gp
 
-def capture(resultFile):
+def capture(downloadPath, resultFile, showImage):
     context = gp.gp_context_new()
     camera = gp.check_result(gp.gp_camera_new())
     gp.check_result(gp.gp_camera_init(camera, context))
@@ -10,12 +10,13 @@ def capture(resultFile):
     file_path = gp.check_result(gp.gp_camera_capture(
         camera, gp.GP_CAPTURE_IMAGE, context))
     print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-    target = resultFile
+    target = downloadPath+"/"+resultFile
     print('Copying image to', target)
-    camera_file = gp.check_result(gp.gp_camera_file_get(
-            camera, file_path.folder, file_path.name,
-            gp.GP_FILE_TYPE_NORMAL, context))
+    camera_file = gp.check_result(gp.gp_camera_file_get(            
+        camera, file_path.folder, file_path.name,
+        gp.GP_FILE_TYPE_NORMAL, context))
     gp.check_result(gp.gp_file_save(camera_file, target))
-    subprocess.call(['xdg-open', target])
+    if (showImage):
+        subprocess.call(['xdg-open', target])
     gp.check_result(gp.gp_camera_exit(camera, context))
     return True
