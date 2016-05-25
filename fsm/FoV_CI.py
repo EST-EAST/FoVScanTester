f40c3fc7a491ee601b527fa1dd82f9c9a8898758
Txinto Vaz <txinto@iac.es>
2016-05-25 16:58:55 +0100

    Mutex added to protect the la and np flags.

--- fsm/FoV_CI.py
+++ fsm/FoV_CI.py
@@ -1,13 +1,17 @@
 import DRE
+from threading import Thread, Lock
 # ['Common definitions for 'Code items generator'' begin (DON'T REMOVE THIS LINE!)]
 # Generic code items' definitions
 dre = DRE.DRE()
 
 def obtainVarName( variable ):
     # ['<global>::obtainVarName' begin]
-    for k, v in locals().items():
-        if v is variable:
-            a_as_str = k
+    for k, v in locals().items():
+
+        if v is variable:
+
+            a_as_str = k
+
     return a_as_str
     # ['<global>::obtainVarName' end]
 
