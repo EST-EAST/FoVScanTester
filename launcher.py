c78d6b558efd9d9eb12c8d5b27ba4342227ec458
Txinto Vaz <txinto@iac.es>
2016-06-02 11:10:30 +0100

    Refactoring details.

--- launcher.py
+++ launcher.py
@@ -7,18 +7,19 @@ Created on Thu May 12 13:13:44 2016
 import os
 import urllib
 import sweepsupport
+import sweepconfig
 
 def import_URL(URL):
     exec urllib.urlopen(URL).read() in globals()
     
-menurl = sweepconfig.cte_web_root+"/sweep_exes"
+menurl = sweepconfig.cte_web_root+"/scan_exes"
 print "Running on "+os.name
 print "The menu chooser will be downloaded from "+menurl+".py"
 print "The list of avaliable algorithms are here: "+menurl
 import_URL(menurl+".py")
 
-if (sweep_id_to_execute != 0):
-    sweepurl=sweepconfig.cte_web_root+"/sweep_exes/"+str(sweep_id_to_execute)
+if (scan_id_to_execute != 0):
+    sweepurl=sweepconfig.cte_web_root+"/scan_exes/"+str(scan_id_to_execute)
     print "The algorithm to be downloaded is "+sweepurl+".py"
     print "It corresponds to the one described at "+sweepurl
     import_URL(sweepurl+".py")
