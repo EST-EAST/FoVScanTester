1ae761abc0d9498b930b461512b034b9817ad743
Txinto Vaz <txinto@ecusw.com>
2016-11-10 18:32:48 +0000

    Gregor calibrations

--- scancalib.py
+++ scancalib.py
@@ -14,21 +14,32 @@ cte_stepTime = cte_waitTime * 1000
 cte_lsx_min = 0  # End of LS travel in lower units
 cte_lsx_scale = (1000 * 1000)  # LS units / mm * 1000 mm / 1 m
 cte_lsx_max = 26900  # End of LS travel in upper units
-cte_lsx_zero = 13100  # LS units coincidence with 0 mm (center)
+#cte_lsx_zero = 13100  # LS units coincidence with 0 mm (center)
+cte_lsx_zero = 11100  # Primer alineado en GREGOR
 
 # My = y
 cte_lsy_min = 0  # End of LS travel in lower units
 cte_lsy_scale = - (2000 * 1000)  # LS units / mm * 1000 mm / 1 m
 cte_lsy_max = 39401  # End of LS travel in upper units
-cte_lsy_zero = 18400  # LS units coincidence with 0 mm (center)
+#cte_lsy_zero = 18400  # LS units coincidence with 0 mm (center)
+cte_lsy_zero = 16400  # Primer alineado en GREGOR
 
 # Mcomp = compensaTion
 cte_lscomp_min = 0  # End of LS travel in lower units
 cte_lscomp_scale = (2000 * 1000)  # LS units / mm * 1000 mm / 1 m
 cte_lscomp_max = 39800  # End of LS travel in upper units
 #cte_lscomp_zero = 25800  # LS units coincidence with 0 mm (center)
-cte_lscomp_zero = 20250  # LS units coincidence with 0 mm (center)
-#bien 20250
+#cte_lscomp_zero = 15000  # LS units coincidence with 0 mm (center)
+#cte_lscomp_zero = 20350# LS units coincidence with 0 mm (center) #CARLOS
+cte_lscomp_zero = 19500 # Primer alineado en GREGOR
+#bien 20250 antes de tcar SM1 y RS1
+#Calculado con RS1 19500 ver excel
+#Calculado con Oct_BancoFocoSW 19650 ver excel
+# Con mascara A kapton F_silica en visible(550nm) aumentar 1mm x (2000)/2 = 1000 entonces quedaria en 20650
+# Con mascara A kapton F_silica en NIR(1um) aumentar 0.7mm x (2000)/2 = 700 entonces quedaria en 20350
+#Calculado en visible con Oct_BancoFocomascaraSW 20500 ver excel
+#TXINTO 19350 # Primer alineado en GREGOR
+#19500 #  Alineado en GREGOR IR
 
 # Home speeds
 cte_vhx = 100
