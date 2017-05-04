5a0adcfeabe91824df513ac67147f6fd31b74728
Txinto Vaz <txinto@iac.es>
2017-05-04 11:31:38 +0100

    Translate repeatability software to english

--- repeat_goprefixpos.py
+++ repeat_goprefixpos.py
@@ -1,9 +1,9 @@
 # Support script for repeatability tests
 # Moves system to "prefix" position
-# TODO: translate to english
+
 
 import scansupport as ss
 from scancalib import *
 from repet_ceros import *
 
-ss.commandMotor(0.0, valor_prefijo)
+ss.commandMotor(0.0, repeat_prefix_pos)
