01f4f4b16e47d7881d4f8abda22ac0fc8d35d11b
Txinto Vaz <txinto@iac.es>
2017-05-16 11:55:11 +0100

    commandMotor.py and commandMotorUnits.py now accept arguments.

--- comandMotor.py
+++ comandMotor.py
@@ -1,4 +1,18 @@
+import sys
 import scansupport as ss
 
+print 'Moving motor to X, Y (in mm)'
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) > 2:
+    x = float(sys.argv[1])/1000.0
+    y = float(sys.argv[2])/1000.0
+else:
+    x = 0.0 / 1000.0
+    y = 0.050 / 1000.0
+
 # We use this command to freely move the window in 2D (in meters) while compensation is calculated
-ss.commandMotor(0.0, 0.000050)
+print 'x (in meters) is ', x
+print 'y (in meters) is ', y
+ss.commandMotor(x, y)
--- comandMotorUnits.py
+++ comandMotorUnits.py
@@ -1,5 +1,24 @@
+import sys
 import scansupport as ss
+
+print 'Moving motor to LsX, LsY, LsComp (in motor units)'
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) > 3:
+    lsx = float(sys.argv[1])
+    lsy = float(sys.argv[2])
+    lscomp = float(sys.argv[3])
+else:
+    lsx = 500.0
+    lsy = 500.0
+    lscomp = 500.0
+
+# We use this command to freely move the window in 2D (in meters) while compensation is calculated
+print 'lsx (in motor units) is ', lsx
+print 'lsy (in motor units) is ', lsy
+print 'lscomp (in motor units) is ', lscomp
 
 # We use this command to move the motors freely in 3D in motor units
 # (in motor units)
-ss.commandMotorUnits3D(0.0, 39000.0, 0.0)
+ss.commandMotorUnits3D(lsx, lsy, lscomp)
